1b566f65ce3cf303ec69e9510ddbd80bab55c60f
Matthew Beatty <beattyml1@gmail.com>
2016-06-09 00:16:24 -0400

    Create constraints.js

---
+++ constraints.js
@@ -0,0 +1,120 @@
+function Null() {
+	return Value(null);
+}
+
+function And(left, right) {
+	return { 
+		instanceTest: function(instance) {
+			return left.instanceTest(instance) || right.instanceTest(instance);
+		},
+
+		constraintTest: function(constraint) {
+			return left.constraintTest(constraint) || right.constraintTest(constraint);
+		}
+	}
+}
+
+function Or(left, right) {
+	return { 
+		instanceTest: function(instance) {
+			return left.instanceTest(instance) && right.instanceTest(instance);
+		},
+
+		constraintTest: function(constraint) {
+			return left.constraintTest(constraint) && right.constraintTest(constraint);
+		}
+	}
+}
+
+function Record(spec) {
+	return { 
+		instanceTest: function(instance) {
+			for (var specProp in spec) {
+				var propMatches = false;
+				for(var instanceProp in instance) {
+					if (specProp === instanceProp) {
+						propMatches = spec[specProp] && spec[specProp].instanceTest(instance[instanceProp])
+						break;
+					}
+				}
+				if (!propMatches) {
+					return false;
+				}
+			}
+		},
+
+		constraintTest: function(constraint) {
+
+		}
+	}
+}
+
+function Any() {
+	return { 
+		instanceTest: function(instance) {
+			
+		},
+
+		constraintTest: function(constraint) {
+
+		}
+	}
+}
+
+function Function(returnConstraint, paramsConstraint) {
+	return { 
+		instanceTest: function(instance) {
+			
+		},
+
+		constraintTest: function(constraint) {
+
+		}
+	}
+}
+
+function Returns(returnConstraint){
+	return { 
+		instanceTest: function(instance) {
+			
+		},
+
+		constraintTest: function(constraint) {
+
+		}
+	}
+}
+
+function Params(spec) {
+	return { 
+		instanceTest: function(instance) {
+			
+		},
+
+		constraintTest: function(constraint) {
+
+		}
+	}
+}
+
+function Value(value) {
+	return { 
+		instanceTest: function(instance) {
+			return value === instance;
+		},
+
+		constraintTest: function(constraint) {
+
+		}
+	}
+}
+
+function Type(type) {
+	
+}
+
+int = "int";
+
+function Int(){
+	return Type(int);
+}
